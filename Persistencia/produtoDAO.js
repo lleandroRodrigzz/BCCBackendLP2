c98ae57180fdb563f54d7fd64f16b3bc89ac521f
lleandroRodrigzz <leandromarcoscasse@gmail.com>
2024-11-07 22:36:36 -0300

    Tirando data pra varchar na DATA

--- Persistencia/produtoDAO.js
+++ Persistencia/produtoDAO.js
@@ -20,7 +20,7 @@ export default class ProdutoDAO {
                 prod_precoVenda DECIMAL(10,2) NOT NULL,
                 prod_qtdEstoque INT NOT NULL DEFAULT 0,
                 prod_urlImagem VARCHAR(250),
-                prod_dataValidade DATE NOT NULL,
+                prod_dataValidade VARCHAR(10) NOT NULL,
                 fk_codigo_cat INT NOT NULL,
                 CONSTRAINT pk_produto PRIMARY KEY(prod_codigo),
                 CONSTRAINT fk_categoria FOREIGN KEY(fk_codigo_cat) REFERENCES categoria(codigo) 
@@ -38,7 +38,7 @@ export default class ProdutoDAO {
         if (produto instanceof Produto) {
             const conexao = await conectar();
             const sql = `INSERT INTO produto(prod_descricao,prod_precoCusto,prod_precoVenda,prod_qtdEstoque,prod_urlImagem,prod_dataValidade, fk_codigo_cat)
-                values(?,?,?,?,?,str_to_date(?,'%d/%m/%Y'),?)
+                values(?,?,?,?,?,?,?)
             `;
             let parametros = [
                 produto.descricao,
@@ -57,7 +57,7 @@ export default class ProdutoDAO {
     async alterar(produto) {
         if (produto instanceof Produto) {
             const conexao = await conectar();
-            const sql = `UPDATE produto SET prod_descricao=?,prod_precoCusto=?,prod_precoVenda=?,prod_qtdEstoque=?,prod_urlImagem=?,prod_dataValidade=str_to_date(?,'%d/%m/%Y'), fk_codigo_cat = ?
+            const sql = `UPDATE produto SET prod_descricao=?,prod_precoCusto=?,prod_precoVenda=?,prod_qtdEstoque=?,prod_urlImagem=?,prod_dataValidade=?,fk_codigo_cat=?
                 WHERE prod_codigo = ?
             `;
             let parametros = [
